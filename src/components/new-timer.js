950ebf2e93b2bd189265b5529181462835d3bce2
sprajjwal <shashwatprajjwal@gmail.com>
2020-04-21 16:53:50 -0700

    added new timer input

---
+++ src/components/new-timer.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+// We need to import our action to add a new timer
+import { addTimer } from '../actions'
+
+class NewTimer extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { name: "" }
+  }
+
+  render() {
+    // Build out a component that takes a name as input and a save button to save the timer
+      return (
+        <div>
+          <input
+            type='text'
+            placeholder="New Timer Name"
+            name="name"
+            value={this.state.name}
+            onChange={(e) => this.setState({ name: e.target.value })}/>
+          <button
+            onClick={(e) => {
+              this.props.addTimer(this.state.name)
+            }}>Save</button>
+        </div>
+      )
+    }
+}
+
+// Not needed for this component
+const mapStateToProps = (state) => {
+  return {}
+}
+
+// Use 'mapDispatchToProps' to connect the 'newTimer' action creator to this component. Here is a stub for the component:
+const mapDispatchToProps = () => {
+  return { addTimer }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps())(NewTimer)
